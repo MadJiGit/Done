18284c67ea0067e5c34c9036564d49b589b86faf
MadJiGit <raykovmladen@gmail.com>
2021-03-29 22:20:40 +0300

    add header

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header'
 
 
 export default function App() {
+    const [todos, setTodos] = useState([
+        { text: 'bys mth', key: '1' },
+        { text: 'aaaaaaaa', key: '2' },
+        { text: 'swjhgfehqifh', key: '3' },
+    ]);
 
-  return (
-    <View style={styles.container}>
-
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Header /> 
+            <View style={styles.content}>
+                {/* to form */}
+                <View style={styles.list}>
+                    <FlatList 
+                        data={todos}
+                        renderItem={( { item } ) => (
+                            <Text>{item.text}</Text>
+                        )}
+                    />
+                </View>
+            </View> 
+        </View>
+    );
 } 
 
 const styles = StyleSheet.create({   
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    content: {
+        padding: 40,
+    },
+    list: {
+        marginTop: 20,
+    },
 });
  
